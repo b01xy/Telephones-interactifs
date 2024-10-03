f31e64f9910b84e5db48f92f3ad6e8051463da70
b01xy <1310803+b01xy@users.noreply.github.com>
2024-10-03 22:59:54 +0200

    Mise à jour version ok

--- installTelephones.sh
+++ installTelephones.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 sudo apt update
 sudo apt upgrade
 sudo apt install puredata pd-ggee pd-zexy pd-cyclone python3-rpi.gpio git htop
@@ -6,5 +6,8 @@ cd ~/Desktop
 rm -rf Telephones-interactifs/
 git clone https://github.com/b01xy/Telephones-interactifs/
 cd Telephones-interactifs
-
+sudo cp telephone.service /etc/systemd/system/telephone.service
+sudo systemctl enable telephone.service
+sudo systemctl daemon-reload
+sudo systemctl start telephone.service
 exit 0
